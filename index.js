ffe0f0c0c2be3e5160f22339b596105368551386
Adarsha Shrestha <adarsha@Adarshas-MacBook-Pro.local>
2024-06-24 23:03:04 +0800

    login panel username should be adarsha

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require("express"); // importing express
 const hbs = require("hbs"); // importing hbs
 const app = express(); // creating express js app
+const bodyParser = require("body-parser"); //body parser for form data
 
 // view engine setting as hbs
 app.set("view engine", "hbs");
@@ -10,13 +11,23 @@ app.set("view engine", "hbs");
 //setting path for view
 app.set("views", "./views");
 
+// using bodyparser to extract form data.
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Route  with dynamic data passing
 app.get("/", (req, res) => {
-  const data = {
-    name: "adarsha",
-  };
   // render home templete with dynamic data
-  res.render("home", data);
+  res.render("home");
+});
+
+app.post("/login", (req, res) => {
+  const name = req.body.username;
+  const password = req.body.password;
+  if (name === "adarsha" && password === "adarsha") {
+    res.render("dashboard", { name, password });
+  } else {
+    res.send("Login failed");
+  }
 });
 
 //starting a server on port 8000
