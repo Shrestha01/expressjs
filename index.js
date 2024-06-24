5b098cfc8ea172bf51f176980dd1737e9ce36daf
Adarsha Shrestha <adarsha@Adarshas-MacBook-Pro.local>
2024-06-24 22:05:20 +0800

    push

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const express = require("express");
-const app = express();
 
-//Route
-app.get("/", (req, res) => {
-  res.send("Hello i am from serverside");
-});
-app.get("/home", (req, res) => {
-  res.send("Hi, I am from Home page.");
-});
-app.get("/about", (req, res) => {
-  res.send("Hi, I am from About page.");
-});
-
-app.listen("8000", () => {
-  console.log("server running on 8000 port");
-});
