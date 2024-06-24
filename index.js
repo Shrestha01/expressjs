7be42646ee9b131bc7a26e3daf503ed3877b8a96
Adarsha Shrestha <adarsha@Adarshas-MacBook-Pro.local>
2024-06-24 21:58:10 +0800

    route commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello World");
+const express = require("express");
+const app = express();
+
+//Route
+app.get("/", (req, res) => {
+  res.send("Hello i am from serverside");
+});
+app.get("/home", (req, res) => {
+  res.send("Hi, I am from Home page.");
+});
+app.get("/about", (req, res) => {
+  res.send("Hi, I am from About page.");
+});
+
+app.listen("8000", () => {
+  console.log("server running on 8000 port");
+});
