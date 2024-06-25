02c1b60ff4257a00c5c713e634b9d18cc34f64ee
Adarsha Shrestha <adarsha@Adarshas-MacBook-Pro.local>
2024-06-25 20:17:18 +0800

    route level middleware 2nd push

--- index.js
+++ index.js
@@ -4,24 +4,22 @@
 // importing express module
 const express = require("express");
 const app = express();
+// importing middleware function from middleware.js file
+const reqFilter = require("./middleware.js");
 
 //importing router module for route level middleware
 const route = express.Router();
 
-// middleware function
-const reqFilter = (req, res, next) => {
-  if (req.query.age > 18) {
-    next();
-  } else {
-    res.send("Sorry you are under age");
-  }
-};
 //app.use(reqFilter);
 // using middleware function in route
 route.use(reqFilter);
+
+// Route without middleware are created with app.get()
+// Route with middleware are created with route.get()
 app.get("/", (req, res) => {
   res.send("Hello I am from Home page");
 });
+// route with middleware
 route.get("/about", (req, res) => {
   res.send("Hello I am from About page");
 });
