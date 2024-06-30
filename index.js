82d9ac302f167f9f645ba058f3c4a7cadeeb137e
Adarsha <adarsha.stha123@gmail.com>
2024-06-30 16:30:58 +0800

    post api tutorial

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ const app = express();
 app.get("/", (req, res) => {
   con.query("select * from users", (err, result) => {
     if (err) {
-      res.send("error");
+      res.send("error in api");
     } else {
       res.send(result);
     }
+  });
+});
+
+app.post("/", (req, res) => {
+  const data = { id: 2, name: "adarsha", password: "adarsha" };
+
+  con.query("insert INTO users SET ?", data, (err, result, fields) => {
+    if (err) {
+      console.error("Error inserting into users table:", err);
+      return res.status(500).send("Error inserting data into users table");
+    }
+    console.log("Insertion successful");
+    res.send(result);
   });
 });
 
